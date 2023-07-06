854d8fab804525dcc188a288c2aaae396966ed6f
ecruz17 <ecruz17@ucol.mx>
2023-07-06 13:26:58 -0600

    Create first booking steps screens
    Co-authored-by: Lizzflores <lizflores.dev@gmail.com>
    Co-authored-by: CUBOFIG <figmiche@gmail.com>
    Co-authored-by: ShadowEnchanter <enlace.gustavolopez@gmail.com>

---
+++ reservation/src/screens/BookingScreen/DestinationScreen.tsx/styles.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#FFFFFF',
+  },
+  header: {
+    fontSize: 30,
+    color: '#000000',
+    fontWeight: '800',
+    marginBottom: 20,
+  },
+});
